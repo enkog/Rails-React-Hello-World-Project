c5e4a58636b95be016303fbbc0cbe534ee053b4d
enkog <nkirukaogu@gmail.com>
2021-11-10 10:32:28 +0100

    Create a greetings controller and the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/index'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
